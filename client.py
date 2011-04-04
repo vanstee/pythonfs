35b628e54848c2592927791403b510874c2f99f9
Patrick Van Stee <patrickvanstee@gmail.com>
2011-04-03 20:22:14 -0400

    Fix option parsing

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
-import  fuse, xmlrpclib, pickle
+import  fuse, xmlrpclib, pickle, optparse
 
 fuse.fuse_python_api = (0, 2)
 
 class PythonFS(fuse.Fuse):
-  def __init__(self, *args, **kwargs):
+  def __init__(self, proxy, *args, **kwargs):
     fuse.Fuse.__init__(self, *args, **kwargs)
     self.proxy = proxy    
     
@@ -12,4 +12,12 @@ class PythonFS(fuse.Fuse):
     return lambda *args, **kwargs: pickle.loads(func(*args, **kwargs))
 
 proxy = xmlrpclib.ServerProxy('http://localhost:7388', allow_none=True)
-fs = PythonFS(proxy, usage='')
+
+parser = optparse.OptionParser()
+parser.set_usage('python client.py [options] volume1')
+parser.add_option('-m', '--mount-point', dest='mount_point', help='Path to mount raw device', metavar='PATH', default='.')
+options, args = parser.parse_args()
+
+sys.argv = [sys.argv[0], options.mount_point, '-f']
+
+fs = PythonFS(proxy, args, usage='USAGE', version=fuse.__version__)
